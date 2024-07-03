90fc68936e34cbfec2930809585769a690b395ab
Venki1402 <saivenkatesh.alampally@gmail.com>
2024-07-03 11:40:36 +0530

    logger and middleware added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
+app.use(logger);
+app.use(middleware);
 
 let courses = [
   { id: 1, name: "java" },
@@ -22,24 +24,34 @@ app.post("/courses", (req, res) => {
 });
 
 app.put("/courses/:id", (req, res) => {
-    const courseId = parseInt(req.params.id);
-    const courseIndex = courses.findIndex(course => course.id === courseId);
-    if (courseIndex === -1) {
-        return res.status(404).json({ error: "Course not found" });
-    }
-    courses[courseIndex].name = req.body.name;
-    res.json(courses);
+  const courseId = parseInt(req.params.id);
+  const courseIndex = courses.findIndex((course) => course.id === courseId);
+  if (courseIndex === -1) {
+    return res.status(404).json({ error: "Course not found" });
+  }
+  courses[courseIndex].name = req.body.name;
+  res.json(courses);
 });
 
 app.delete("/courses/:id", (req, res) => {
-    const courseId = parseInt(req.params.id);
-    const courseIndex = courses.findIndex(course => course.id === courseId);
-    if (courseIndex === -1) {
-        return res.status(404).json({ error: "Course not found" });
-    }
-    courses.splice(courseIndex, 1);
-    res.json(courses);
+  const courseId = parseInt(req.params.id);
+  const courseIndex = courses.findIndex((course) => course.id === courseId);
+  if (courseIndex === -1) {
+    return res.status(404).json({ error: "Course not found" });
+  }
+  courses.splice(courseIndex, 1);
+  res.json(courses);
 });
+
+function middleware(req, res, next) {
+  console.log("middleware called");
+  next();
+}
+
+function logger(req, res, next) {
+  console.log(req.method, req.ip, req.hostname, req.url, new Date());
+  next();
+}
 
 app.listen(3000, () => {
   console.log("server started");
