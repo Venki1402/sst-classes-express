779c1972ca0af30189f97c5279b815195a0a7a96
Venki1402 <saivenkatesh.alampally@gmail.com>
2024-07-03 10:43:07 +0530

    DELETE Request is functional

--- index.js
+++ index.js
@@ -21,6 +21,16 @@ app.post("/courses", (req, res) => {
   res.json(courses);
 });
 
+app.delete("/courses/:id", (req, res) => {
+    const courseId = parseInt(req.params.id);
+    const courseIndex = courses.findIndex(course => course.id === courseId);
+    if (courseIndex === -1) {
+        return res.status(404).json({ error: "Course not found" });
+    }
+    courses.splice(courseIndex, 1);
+    res.json(courses);
+});
+
 app.listen(3000, () => {
   console.log("server started");
 });
